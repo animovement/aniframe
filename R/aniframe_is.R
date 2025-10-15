cd093591faee1ffdee9e9195b5673f39b331b55d
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2025-10-15 16:47:17 +0200

    Add ensure_is_aniframe

--- R/aniframe_is.R
+++ R/aniframe_is.R
@@ -6,3 +6,10 @@
 is_aniframe <- function(x) {
   inherits(x, "aniframe")
 }
+
+#' @keywords internal
+ensure_is_aniframe <- function(data){
+  if (!is_aniframe(data)){
+    cli::cli_abort("Data is not an aniframe.")
+  }
+}
