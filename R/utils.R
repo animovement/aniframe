bac69d196911da33e2c23c35c529782ee3839bec
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2025-12-01 10:24:58 +0100

    Remove spatial functionality

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,4 @@
+#' @keywords internal
 convert_nan_to_na <- function(data) {
   dplyr::mutate(
     data,
@@ -5,4 +6,22 @@ convert_nan_to_na <- function(data) {
       ifelse(is.nan(x), NA, x)
     })
   )
+}
+
+#' Convert radians to degrees
+#'
+#' @param x Numeric vector of angles (radians).
+#' @return Numeric vector of angles expressed in degrees.
+#' @export
+rad_to_deg <- function(x) {
+  (x * 180) / pi
+}
+
+#' Convert degrees to radians
+#'
+#' @param x Numeric vector of angles (degrees).
+#' @return Numeric vector of angles expressed in radians.
+#' @export
+deg_to_rad <- function(x) {
+  (x * pi) / 180
 }
