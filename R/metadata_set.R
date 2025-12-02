2cef1255453b73f4bd5a4e17d6316823fa949b3a
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2025-12-02 17:08:44 +0100

    Don't set class for NA datetime

--- R/metadata_set.R
+++ R/metadata_set.R
@@ -95,7 +95,7 @@ set_metadata <- function(data, ..., metadata = NULL) {
             levels = levels(defaults[[n]])
           )
         }
-      } else if (n %in% names(defaults) && is_class(defaults[[n]], "POSIXct")) {
+      } else if (n %in% names(defaults) && is_class(defaults[[n]], "POSIXct") && !is.na(user_md[[n]])) {
         user_md[[n]] <- anytime::anytime(user_md[[n]])
       }
     }
